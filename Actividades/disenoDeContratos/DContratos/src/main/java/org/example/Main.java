22d5a9f6ca8bfac5aefa28386842f50f76b8978e
JhonPoma <jpm11797@gmail.com>
2023-06-20 18:07:13 -0500

    holaaaaaa

--- Actividades/disenoDeContratos/DContratos/src/main/java/org/example/Main.java
+++ Actividades/disenoDeContratos/DContratos/src/main/java/org/example/Main.java
@@ -5,5 +5,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("holaaaaaaa");
+
+
+        System.out.println("sdaslfldlairgl");
     }
 }

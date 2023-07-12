0d94fb7d508bff1ff9da191a99bf20b52972a651
JhonPoma <jpm11797@gmail.com>
2023-07-12 09:28:10 -0500

    ExamenFinal

--- ExamenFinal-3S2/Pregunta1/src/main/java/fase2/Flight.java
+++ ExamenFinal-3S2/Pregunta1/src/main/java/fase2/Flight.java
@@ -9,12 +9,11 @@ import java.util.List;
 public abstract class Flight {
 
     private String id;
-    private List<Passenger> passengers = new ArrayList<Passenger>();
+    public List<Passenger> passengers = new ArrayList<Passenger>();
     //private String flightType;
 
     public Flight(String id) {
         this.id = id;
-
     }
 
     public String getId() {
@@ -25,38 +24,14 @@ public abstract class Flight {
         return Collections.unmodifiableList(passengers);
     }
 
-    public String getFlightType() {
-        return flightType;
-    }
+//    public String getFlightType() {
+//        return flightType;
+//    }
 
     public boolean addPassenger(Passenger passenger) {
-        switch (flightType) {
-            case "Economica":
-                return passengers.add(passenger);
-            case "Negocios":
-                if (passenger.isVip()) {
-                    return passengers.add(passenger);
-                }
-                return false;
-            default:
-                throw new RuntimeException("Tipo desconocido: " + flightType);
-        }
-
     }
 
     public boolean removePassenger(Passenger passenger) {
-        switch (flightType) {
-            case "Economica":
-                if (!passenger.isVip()) {
-                    return passengers.remove(passenger);
-                }
-                return false;
-            case "Negocios":
-                return false;
-
-            default:
-                throw new RuntimeException("Tipo desconocido: " + flightType);
-        }
     }
 
 

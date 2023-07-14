3407dd2fe6a0cc465be579283bed240da210478e
JhonPoma <jpm11797@gmail.com>
2023-07-13 19:14:13 -0500

    ExamenFinal

---
+++ ExamenFinal-3S2/Pregunta1/Antes/src/main/java/Airport.java
@@ -0,0 +1,28 @@
+public class Airport {
+
+    public static void main(String[] args) {
+        Flight economyFlight = new Flight("1", "Economico");
+        Flight businessFlight = new Flight("2", "Negocios");
+
+        Passenger checha = new Passenger("Checha", true);
+        Passenger lore = new Passenger("Lore", false);
+
+
+        businessFlight.addPassenger(checha);
+        businessFlight.removePassenger(checha);
+        businessFlight.addPassenger(lore);
+        economyFlight.addPassenger(lore);
+
+        System.out.println(" Lista de pasajeros de vuelos de negocios:");
+        for (Passenger passenger : businessFlight.getPassengersList()) {
+            System.out.println(passenger.getName());
+        }
+
+        System.out.println(" Lista de pasajeros de vuelos economicos:");
+        for (Passenger passenger : economyFlight.getPassengersList()) {
+            System.out.println(passenger.getName());
+        }
+
+    }
+
+}

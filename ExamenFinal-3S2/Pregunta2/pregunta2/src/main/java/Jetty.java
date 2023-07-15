f9a396d8e7cc2616b5ec0f0dacfcd57ccf7c7b31
JhonPoma <jpm11797@gmail.com>
2023-07-14 22:28:24 -0500

    Examen Final

--- ExamenFinal-3S2/Pregunta2/pregunta2/src/main/java/Jetty.java
+++ ExamenFinal-3S2/Pregunta2/pregunta2/src/main/java/Jetty.java
@@ -1,6 +1,14 @@
+import org.mortbay.jetty.HttpHeaders;
 import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.AbstractHandler;
 import org.mortbay.jetty.handler.ResourceHandler;
 import org.mortbay.jetty.servlet.Context;
+import org.mortbay.util.ByteArrayISO8859Writer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class Jetty {
     public static void main(String[] args) throws Exception {
@@ -13,4 +21,18 @@ public class Jetty {
         server.setStopAtShutdown(true);
         server.start();
     }
+
+    private static class TestGetContentOkHandler extends AbstractHandler {
+        public void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch)
+                throws IOException {
+            OutputStream out = response.getOutputStream();
+            ByteArrayISO8859Writer writer = new ByteArrayISO8859Writer ();
+            writer.write("Esto funciona");
+            writer.flush();
+            response.setIntHeader(HttpHeaders.CONTENT_LENGTH, writer.size());
+            writer.writeTo(out);
+            out.flush();
+        }
+    }
+
 }
